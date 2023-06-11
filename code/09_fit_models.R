7b5428b7bd39217ed2e0667fbbc4c6adb2c0474a
Matt Ashby <mpjashby@gmail.com>
2023-06-11 14:44:46 +0100

    Add downtown overall model

--- code/09_fit_models.R
+++ code/09_fit_models.R
@@ -27,7 +27,12 @@ data_venue <- here("analysis_data/model_data_venue.csv.gz") |>
 
 data_downtown <- here("analysis_data/model_data_downtown.csv.gz") |>
   read_csv() |>
-  filter(offence_type == "assault")
+  filter(offence_type == "assault") |>
+  mutate(crime_count_lag = lag(crime_count), .by = city) |>
+  mutate(
+    daylight_mins = as.numeric(scale(daylight_mins)),
+    sport = fct_relevel(sport, "none")
+  )
 
 data_restofcity <- here("analysis_data/model_data_rest_of_city.csv.gz") |>
   read_csv() |>
@@ -108,6 +113,35 @@ model_venue_day_time <- brm(
   # Specify starting value for MCMC chains (as recommended in `?mixture`)
   init = 0
 )
+
+
+
+# DOWNTOWN MODELS --------------------------------------------------------------
+
+
+## Model for venues on all days/times ----
+model_downtown_all <- brm(
+  formula = crime_count ~ crime_count_lag +
+    (1 + daylight_mins + sport | city),
+  data = data_downtown,
+  # Use a negative binomial model since the dependent variable is a count
+  # variable with lots of zeros
+  family = negbinomial(),
+  prior = set_prior("normal(0,0.1)", class = "b", coef = "crime_count_lag"),
+  # Run the chains in parallel using multiple processor cores (there are four
+  # chains by default, so using more than four cores would not speed up the
+  # model estimation any further)
+  cores = 4,
+  # Save a copy of the model in a `.rds` file (`brm()` will add the `.rds`
+  # file extension)
+  file = here("analysis_data/model_downtown_all"),
+  # Re-estimate the model if the specification has changed relative to the model
+  # saved in the above file, otherwise re-load the model from the file
+  file_refit = "on_change",
+  # Specify starting value for MCMC chains (as recommended in `?mixture`)
+  init = 0
+)
+
 
 
 # For advice on interpreting diagnostic plots for this model, see
